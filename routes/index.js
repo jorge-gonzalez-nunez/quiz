6b9b4138b673877f534cc4454dafc879ab9b18a0
jorge-gonzalez-nunez <jorge.gonzalez.nunez@alumnos.upm.es>
2015-05-12 13:20:48 +0200

    Merge con autores

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-<<<<<<< HEAD
+
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
@@ -16,7 +16,7 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Autores' });
 });
->>>>>>> autores
+
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
