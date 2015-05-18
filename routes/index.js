f60e5332b427e2aea34d9a718f67cefc09df7077
jorge-gonzalez-nunez <jorge.gonzalez.nunez@alumnos.upm.es>
2015-05-18 19:58:59 +0200

    Crear comentario

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
@@ -12,13 +13,14 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
-
+//Autores
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Autores' });
 });
 
 
 // Definición de rutas de /quizes
+
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
@@ -27,5 +29,8 @@ router.post('/quizes/create',              quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
 
 module.exports = router;
